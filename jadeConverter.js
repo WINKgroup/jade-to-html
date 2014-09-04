de286982a6e4c7d973ab3bbd9c39f68d4d7dd121
Andrea Giglio <andrea-giglio@live.it>
2014-09-04 10:05:55 +0200

    Added readme and package.json

--- jadeConverter.js
+++ jadeConverter.js
@@ -3,31 +3,31 @@ var jade = require('jade'),
     glob = require("glob")
 
 // config
-var folderName = "jadeConverted";
+var folderDestinationName = "jadeConverted",
+    filesPath = "**/*";
 
 // create file folder  named : jadeConverted
 var mkdirSync = function (path) {
     try {
-        fs.mkdirSync("./" + folderName);
+        fs.mkdirSync("./" + folderDestinationName);
     } catch (e) {
         if (e.code != 'EEXIST') console.error("Folder exist")
     }
 }();
 // options is optional
-glob("**/*.jade", {}, function (er, files) {
+glob(filesPath+".jade", {}, function (er, files) {
     console.log("Number of jade file in folder " + files.length);
     // files is an array of filenames.
     // If the `nonull` option is set, and nothing
     // was found, then files is ["**/*.js"]
     // er is an error object or null.
     for (var i = 0; i < files.length; i++) {
-        console.log("files[i", files[i])
         var html = jade.renderFile('./' + files[i], {});
-        fs.writeFile("./jadeConverted/" + files[i].split('.')[0] + ".html", html, function (err) {
+        fs.writeFile("./"+folderDestinationName+"/" + files[i].split('.')[0] + ".html", html, function (err) {
             if (err) {
                 console.log(err);
             } else {
-                console.log("The file" + files[i].split('.')[0] + ".html, " + " was saved!");
+                console.log("The file was saved!");
             }
         });
     }
